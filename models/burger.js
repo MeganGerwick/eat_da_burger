6b07bce56b27aa71ec851603cba7e1ae131e5a66
Louisa May <mgerwick@gmail.com>
2020-12-14 11:31:05 -0600

    added first route

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,15 @@
 var orm = require("../config/orm.js");
 
+var burger = {
+    all: function (cb) {
+        orm.selectALL("burger")
+    }
+
+    //add a burger
+
+    //devour burger
+
+    //
+}
 
 module.exports = burger;
