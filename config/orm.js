299d7b687927de1ce39b659cf51a484d81a21410
Louisa May <mgerwick@gmail.com>
2020-12-14 15:56:26 -0600

    fixed orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,37 @@
 var connection = require("./connection.js");
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
 var orm = {
     selectALL: function (whatToSelect, tableInput) {
         var queryString = "SELECT ?? FROM ??";
@@ -8,29 +40,36 @@ var orm = {
             console.log(result);
         });
     },
-    selectONE: function (tableInput, colToSearch, valOfCol) {
-        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    insertONE: function (newBurger) {
+        var queryString = "INSERT INTO burgers";
+        queryString += " (";
+        queryString += newBurger.toString();
+        queryString += ")";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(newBurger.length);
+        queryString += ")";
+
 
         console.log(queryString);
 
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+        connection.query(queryString, [newBurger], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    updateOne: function (whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
-        var queryString = "SELECT ?? FROM ?? AS tOne";
-        queryString += " LEFT JOIN ?? AS tTwo";
-        queryString += " ON tOne.?? = tTwo.??";
+    updateOne: function (table, objColVas, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVas);
+        queryString += " WHERE "
+        queryString += condition;
 
         console.log(queryString);
 
-        connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function (
-            err,
-            result
+        connection.query(queryString, function (err, result
         ) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     }
 };
--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,4 @@
 var orm = require("../config/orm.js");
 
-var burger = {
-    all: function (cb) {
-        orm.selectALL("burger")
-    }
-
-    //add a burger
-
-    //devour burger
-
-    //
-}
 
 module.exports = burger;
